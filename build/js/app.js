336d31cc2e6215a288b125d86cd7ebd71708e1b8
Nicole Freed and Nathan Stewart <nicole@>
2017-07-31 11:47:54 -0700

    add promise and error handling to weather API call

--- build/js/app.js
+++ build/js/app.js
@@ -55,10 +55,18 @@ $(document).ready(function() {
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+
+    // $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+      .then(function(response) {
+        $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+      // console.log("The humidity in " + city + " is " + response.main.humidity + "%");
+    })
+
+    .fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
     });
+    // console.log("Notice: The GET request has been made.");
   });
 });
 
