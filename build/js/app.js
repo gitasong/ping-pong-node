44a3f458dfc1c0dc0013981380b71cc5d8c3e026
Nicole Freed and Nathan Stewart <nicole@>
2017-07-31 11:00:11 -0700

    add API call to get current humidity

--- build/js/app.js
+++ build/js/app.js
@@ -49,4 +49,17 @@ $(document).ready(function() {
   $('#time').text(moment());
 });
 
+var apiKey = "e542d99a4ac0c863239f1e0f26afaf5a";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
+
 },{"./../js/pingpong.js":1}]},{},[2]);
